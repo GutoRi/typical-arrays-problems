2bfcc4db5189283183e8df2216c3a2f71b8ee897
Ilya Gutorov <gutorov.iliya@gmail.com>
2021-08-21 17:39:51 +0300

    feat: add code task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return array == '' || array == undefined ? 0 : Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return array == '' || array == undefined ? 0 : Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == '' || array == undefined) {
+        return 0;
+    }
+
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
 }
